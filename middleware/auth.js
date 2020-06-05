c39b7e5c7005a1a5680b5f4fea37591173ca35e7
Nimzyow <n_soufiani@hotmail.com>
2020-06-05 22:55:07 +0100

    updated middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,18 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const jwtSecret = config.get("jwtSecret");
+const tokenUtiles = require("../tokenUtils");
 
-module.exports = function (req, res, next) {
+module.exports = async (req, res, next) => {
+  // get token from header
   const token = req.header("x-auth-token");
 
+  // check if not token
   if (!token) {
-    return res.status(401).json({ msg: "No token, authorization denied" });
+    return res.status(401).json({ msg: "No token, Authorization denied" });
   }
-
   try {
-    const decoded = jwt.verify(token, jwtSecret);
+    const decoded = await tokenUtiles.verify(token);
     req.user = decoded.user;
     next();
-  } catch (error) {
-    console.error(error.message);
-    res.status(401).json({ msg: "token is not valid" });
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
