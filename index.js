99e63788ad89774fe51c5fa6f0bd41fe191966fa
Nimzyow <n_soufiani@hotmail.com>
2020-06-05 22:52:26 +0100

    updated server routes

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const app = require("./server");
+const db = require("./config/db");
+const path = require("path");
+const express = require("express");
 
-const PORT = 5000 || process.env.PORT;
+db.connect();
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("scuba-diver/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "scuba-diver", "build", "index.html")),
+  );
+}
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
-const app = express();
+const cors = require("cors");
 
-connectDB();
+const app = express();
 
 //initialise middlware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-app.use("/users", require("./routes/users"));
-app.use("/auth", require("./routes/auth"));
-app.use("/dives", require("./routes/dives"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/dives", require("./routes/dives"));
 
 module.exports = app;
