776c5c3929891cf72f8f5e60eec1bcfc3cf415d9
Nimzyow <n_soufiani@hotmail.com>
2020-06-05 22:53:44 +0100

    db now connects to different environments

--- config/db.js
+++ config/db.js
@@ -1,18 +1,79 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const mongoURI = config.get("mongoURI");
 
-const connectDB = async () => {
+const connectDB = async (uri) => {
   try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
+    await mongoose.connect(uri, {
       useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
     });
-    console.log("MongoDB connected");
   } catch (err) {
-    console.err(err.message);
+    // eslint-disable-next-line no-console
+    console.error(err.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+const connect = () => {
+  if (process.env.NODE_ENV === "test") {
+    return connectDB(process.env.scubaDiverTestURI)
+      .then(() => {
+        // eslint-disable-next-line no-console
+        console.log(`Connected to ${process.env.NODE_ENV} db`);
+        // eslint-disable-next-line no-console
+      })
+      .catch((err) => console.log(err.message));
+  }
+  if (process.env.NODE_ENV === "production") {
+    return connectDB(process.env.scubaDiverProductionURI)
+      .then(() => {
+        // eslint-disable-next-line no-console
+        console.log(`Connected to ${process.env.NODE_ENV} db`);
+        // eslint-disable-next-line no-console
+      })
+      .catch((err) => console.log(err.message));
+  }
+  if (process.env.NODE_ENV === "development") {
+    return connectDB(process.env.scubaDiverDevelopmentURI)
+      .then(() => {
+        // eslint-disable-next-line no-console
+        console.log(`Connected to ${process.env.NODE_ENV} db`);
+        // eslint-disable-next-line no-console
+      })
+      .catch((err) => console.log(err.message));
+  }
+  // eslint-disable-next-line no-console
+  console.error(`
+  Couldn't connect:
+  
+  env: ${process.env.NODE_ENV}`);
+  return null;
+};
+
+const disconnect = () => {
+  try {
+    mongoose.connection.close(() => {
+      // eslint-disable-next-line no-console
+      console.log(`Disconnected from ${process.env.NODE_ENV} db`);
+      process.exit(0);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const cleanDatabase = async () => {
+  try {
+    await mongoose.connection.db.dropDatabase();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const db = {
+  connect,
+  disconnect,
+  cleanDatabase,
+};
+
+module.exports = db;
